2c31e079a09c53952bab8bf7a1edc4b1a4b0ef62
jofer86 <jofer86@gmail.com>
2019-09-13 19:16:51 -0500

    Change to the App comp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This, in the near future, will be an awesome CALCULATOR!!
         </p>
         <a
           className="App-link"
