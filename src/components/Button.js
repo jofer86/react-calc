9edcdbb508da43c708bf7875a8c8fb29b2fbbb01
jofer86 <jofer86@gmail.com>
2019-09-20 13:55:24 -0500

    refactor the code, cleaned up the code a lot. Rendered the button panel in a more efficient way.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,30 +3,21 @@ import PropTypes from 'prop-types';
 import './style/Button.css';
 
 const Button = (props) => {
-  const classHandler = (id) => {
-    let output = null;
-    if (id === 17) {
-      output = 'wide';
-    } else if (id === 4 || id === 8 || id === 12 || id === 16 || id === 19) {
-      output = 'orange';
-    } else {
-      output = 'Button';
-    }
-    return output;
-  };
-
   const { name, wide, color } = props;
-  const button = <div className={wide ? 'wide' : 'Button'}>{name}</div>;
+  const width = wide ? '50.7%' : '25%';
+  const button = <div className="Button" style={{ backgroundColor: color, width }}>{name}</div>;
   return button;
 };
 
 Button.defaultProps = {
   wide: false,
+  color: 'orange',
 };
 
 Button.propTypes = {
   name: PropTypes.string,
-  btnStyler: PropTypes.number,
+  wide: PropTypes.bool,
+  color: PropTypes.string,
 };
 
 export default Button;
