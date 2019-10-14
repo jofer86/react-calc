dbff5550f4aca6d56cb114425c4bd5f845e11d15
jofer86 <jofer86@gmail.com>
2019-10-14 11:43:25 -0500

    minor linting

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,15 +1,9 @@
 import operate from './operate';
 
 const Calculate = (obj, buttonName) => {
-  //   //Implement the calculate.js module
-  // It should contain one default method: calculate.
-  // The calculate method should accept a calculator data object and a button name as arguments.
-  // The calculator data object should contain three properties: total, next and operation.
-  // This method should use the buttonName argument to mutate the calculator’s data object and return its modified version. For instance if buttonName == "+/-", it should multiply total and next by -1 to make it negative or positive.
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(buttonName);
   const notZero = buttonName !== '0';
   const operations = ['+', '-', 'X', '%', '=', '+/-'].includes(buttonName);
-  
   let { next, total, operation } = obj;
 
   switch (buttonName) {
