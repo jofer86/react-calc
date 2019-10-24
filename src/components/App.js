ad91169b4d0b1e64a5c31cb3948095eac84e9107
Jorge <jofer86@gmail.com>
2019-10-24 08:36:45 -0500

    chang

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 import '../style/App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-<<<<<<< HEAD
-//windows test
-=======
 import Calculate from '../logic/calculate';
 
->>>>>>> 409976eca418728b2a481f8f3f913f48753acc94
 class App extends Component {
   render() {
     const calc = (
