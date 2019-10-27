28855e7c84f7981dd9d59d15750ac1b24a17c900
Jorge <jofer86@gmail.com>
2019-10-27 16:55:15 -0600

    linting and some small rework

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -5,20 +5,19 @@ const Calculate = (obj, buttonName) => {
   const operations = ['+', '-', 'x', '%', '+/-', '÷'].includes(buttonName);
   const equals = ['='].includes(buttonName);
   let { next, total, operation } = obj;
-  
 
   if (buttonName === 'AC') {
     total = null;
     next = null;
     operation = null;
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (equals) {
     total = operate(total, next, operation);
     next = null;
     operation = null;
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (numbers) {
@@ -31,31 +30,31 @@ const Calculate = (obj, buttonName) => {
 
   if (operations && next && buttonName === '+/-') {
     next = (parseInt(next) * -1).toString();
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (operations && total && buttonName === '+/-') {
     next = (parseInt(total) * -1).toString();
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (operations && next && total && operation) {
     next = operate(total, next, operation);
     total = null;
     operation = null;
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (operations && total) {
     operation = buttonName;
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   if (operations && buttonName !== '+/-' && next) {
     total = next;
     next = null;
     operation = buttonName;
-    return { next, total, operation }
+    return { next, total, operation };
   }
 
   // eslint-disable-next-line consistent-return
